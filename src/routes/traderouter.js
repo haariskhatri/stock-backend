c0a7d1189805f0b1a422cce825a2f01a31d59a89
haariskhatri <haarishkkhatri@gmail.com>
2023-06-01 15:45:11 +0530

    added routers for user and trade

---
+++ src/routes/traderouter.js
@@ -0,0 +1,9 @@
+const { Router } = require("express");
+
+const traderouter = Router();
+
+traderouter.get('/getprice', (req, res) => {
+
+})
+
+module.exports = traderouter;
---
+++ src/routes/userrouter.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const userrouter = express.Router();
+
+userrouter.get('/getuserdetail', async (req, res) => {
+
+})
+
+userrouter.post('/addUser', async (req, res) => {
+    const { userName, userFullname } = req.body;
+
+    const newUser = await addUser(userName, userFullname);
+    res.json(newUser);
+})
+
+
+
+module.exports = userrouter;
